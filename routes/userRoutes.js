31b994151f8bb357ae1b87df63f66de31e31d582
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-06-27 12:14:14 +0530

    Add user routes for dashboard, links, settings, and withdrawals

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { isAuthenticated } = require('../middlewares/authMiddleware');
+const userController = require('../controllers/userController');
+const linkController = require('../controllers/linkController');
+const withdrawalController = require('../controllers/withdrawalController');
+
+router.use(isAuthenticated);
+
+router.get('/dashboard', userController.getDashboard);
+
+router.get('/links', (req, res) => res.render('user/links', { title: 'Manage Links' })); // Links view logic to be added
+router.post('/links/create', linkController.createLink);
+
+router.get('/settings', userController.getSettings);
+router.post('/settings/update', userController.updateProfile);
+
+router.get('/withdrawals', withdrawalController.getWithdrawals);
+router.post('/withdrawals/request', withdrawalController.requestWithdrawal);
+
+module.exports = router;
