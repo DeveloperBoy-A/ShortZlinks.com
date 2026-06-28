91c447b4fff37d23d815fac01f16e6db85f2defa
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-06-28 06:24:43 +0530

    Import apiController in indexRoutes.js

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const apiController = require("../controllers/apiController");
 
 router.get('/', (req, res) => {
     if (req.session.user) {
